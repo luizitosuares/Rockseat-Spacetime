dc99e905b94ed9f8fa966f947c2399ec31357fb2
luizitosuares <luizito.dev@gmail.com>
2023-06-04 16:50:03 -0300

    parte dois aula 1

---
+++ web/src/app/components/Button.tsx
@@ -0,0 +1,11 @@
+const styles = {
+    color: '#F00',
+}
+
+const nome = 'NELSON'
+
+export function Button(){
+return (
+    <p style={styles}>{nome} </p>
+)
+}
---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/Button"
+
+export default function Home() {
+  return (
+  <h1> Olá, Nelson
+    <Button/>
+  </h1>
+    )}
+
